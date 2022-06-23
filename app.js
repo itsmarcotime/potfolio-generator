795640ad9f9956f1980c1b500478a64a83d9cb21
Marco Mata <patrickstar1281@yahoo.com>
2022-06-23 05:52:28 -0500

    added html using node

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+
+});
+
+
+
+
+/* console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
 
@@ -19,3 +32,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
